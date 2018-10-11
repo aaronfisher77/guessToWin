f2a809269195e1539d89d3459cf1b0e0dfc464d0
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-10-11 14:59:44 -0500

    Fixed a few more bugs that will allow for a more enjoyable experience

--- guessToWin/Game.swift
+++ guessToWin/Game.swift
@@ -16,39 +16,51 @@ class Game: UIViewController {
     @IBOutlet weak var chosenRange: UILabel!
     @IBOutlet weak var guessesUsed: UILabel!
     @IBOutlet weak var guessesRemaining: UILabel!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var playAgain: UIButton!
     
     // Creates vaiables that allows extra information in the game and sets different aspecs (difficulty, Selecting the random number, and "deciding" how many guesses will be alloted
     var usedCounter: Int = 0
     var passedData: (difficulty: Int, randomNumber: Int, counter: Int) = (0,0,0)
     
+    
     // This override functions allows the screen to load up before the user sees it.
     override func viewDidLoad() {
         super.viewDidLoad()
-        chosenRange.text = String("Guess a number from 0 to \(passedData.difficulty)") // Tells the used what range to guess from
+        chosenRange.text = String("Guess a number from 0 to \(passedData.randomNumber)") // Tells the used what range to guess from
         guessesRemaining.text = ("Guesses Remaining: \(passedData.counter)") // Tells the user how many guesses they will recieve.
+        playAgain.isHidden = true
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func submitButton(_ sender: Any) { // This will tell the computer to check the Users input and see how it should respond to the number it was given.
-        if Int(usersGuess.text!) != nil { // this wikl verify that the number was uin fact a number.
-            if Int(usersGuess.text!)! > passedData.difficulty && Int(usersGuess.text!)! < 0 { // this checkes to make sure the number was inside of the given range.
+        if Int(usersGuess.text!) != nil { // This will verify that the number was in fact a number.
+            if Int(usersGuess.text!)! > passedData.difficulty && Int(usersGuess.text!)! < 0 { // This checkes to make sure the number was inside of the given range.
                 InfoButton.text = ("Please choose a number inside the current range.")
-                }else if Int(usersGuess.text!) == passedData.randomNumber { // this will check to see if the users number is in fact the number that was randomly chosen at in the ViewController file.
+                }else if Int(usersGuess.text!) == passedData.randomNumber { // This will check to see if the users number is in fact the number that was randomly chosen at in the ViewController file.
                     InfoButton.text = ("You guessed it Congratulations!")
-                }else if Int(usersGuess.text!)! < passedData.randomNumber &&  passedData.counter >= 1{
+                    stackView.isHidden = true
+                    playAgain.isHidden = false
+                    
+                
+            
+                }else if Int(usersGuess.text!)! < passedData.randomNumber &&  passedData.counter >= 1{ // Checks to see if the number you chose is lower than the random number.
                     InfoButton.text = ("Nope, thats to low.")
-                    passedData.counter -= 1
+                    passedData.counter -= 1 // Takes one away from the the Users remaining guesses
                     guessesRemaining.text = ("Guesses Used: \( passedData.counter)")
-                    usedCounter += 1
+                    usedCounter += 1 // Adds one to the amount of guesses the user has used
                     guessesUsed.text = ("Guesses Used: \(usedCounter)")
-                }else if Int(usersGuess.text!)! > passedData.randomNumber &&  passedData.counter >= 1{
+                    usersGuess.text = ("") // Sets the user input back to nothing so that the user doesnt have to press bak on their previous guess
+                
+                }else if Int(usersGuess.text!)! > passedData.randomNumber &&  passedData.counter >= 1{// Checks to see if the number you chose is higher than the random number.
                     InfoButton.text = ("Nope thats too high")
-                    passedData.counter -= 1
+                    passedData.counter -= 1 // Takes one away from the the Users remaining guesses
                     guessesRemaining.text = ("Guesses Used: \( passedData.counter)")
-                    usedCounter += 1
+                    usedCounter += 1 // adds one to the amount of guesses the user has used
                     guessesUsed.text = ("Guesses Used: \(usedCounter)")
-                }else{
+                    usersGuess.text = ("") // Sets the user input back to nothing so that the user doesnt have to press bak on their previous guess
+                }else{ // When the user runs out of guesses the text becomes 0 and it prints the text below...
                     usersGuess.text = ("-")
                     InfoButton.text = ("Well thats to Bad you lost")
                 }
--- guessToWin/ViewController.swift
+++ guessToWin/ViewController.swift
@@ -10,24 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var difficultyAndRandomNumberAndCounter = (difficulty: 0, randomNumber: 0, counter: 0)
+    var difficultyAndRandomNumberAndCounter = (difficulty: 0, randomNumber: 0, counter: 0) //Creates a tuple that will pass through the difficulty, random number, and counter.
     
-    @IBAction func easy(_ sender: Any) {
-        difficultyAndRandomNumberAndCounter.difficulty = 50
-        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...50)
-        difficultyAndRandomNumberAndCounter.counter = 10
+    @IBAction func easy(_ sender: Any) { // This will set the requirements that will make easy...easy.
+        difficultyAndRandomNumberAndCounter.difficulty = 50 // Sets difficulty
+        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...50) // Sets random number 0 - 50
+        difficultyAndRandomNumberAndCounter.counter = 10 // Sets number of guesses
     }
     
-    @IBAction func medium(_ sender: Any) {
-        difficultyAndRandomNumberAndCounter.difficulty = 100
-        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...100)
-        difficultyAndRandomNumberAndCounter.counter = 7
+    @IBAction func medium(_ sender: Any) { // This will set the requirements that will make medium...medium.
+        difficultyAndRandomNumberAndCounter.difficulty = 100 // Sets difficulty
+        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...100) // Sets random number 0 - 100
+        difficultyAndRandomNumberAndCounter.counter = 7 // Sets number of guesses
     }
     
-    @IBAction func hard(_ sender: Any) {
-        difficultyAndRandomNumberAndCounter.difficulty = 200
-        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...200)
-        difficultyAndRandomNumberAndCounter.counter = 5
+    @IBAction func hard(_ sender: Any) { // This will set the requirements that will make hard...hard.
+        difficultyAndRandomNumberAndCounter.difficulty = 200 // Sets difficulty
+        difficultyAndRandomNumberAndCounter.randomNumber = Int.random(in: 0...200) // Sets random number 0 - 200
+        difficultyAndRandomNumberAndCounter.counter = 5 // Sets number of guesses
     }
     
     override func viewDidLoad() {
@@ -37,7 +37,7 @@ class ViewController: UIViewController {
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?)
     {
-        if segue.destination is Game
+        if segue.destination is Game // Sets the destination of the information
         {
             let vc = segue.destination as? Game
             vc?.passedData = difficultyAndRandomNumberAndCounter
