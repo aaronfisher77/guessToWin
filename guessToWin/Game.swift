baa608c8a727f74190a202046a278e614d31aeab
Aaron Fisher <aaronfisher@MacBook-Pro-20.local>
2018-10-15 12:42:15 -0500

    Fixed a few bugs and some constraints.

--- guessToWin/Game.swift
+++ guessToWin/Game.swift
@@ -18,6 +18,9 @@ class Game: UIViewController {
     @IBOutlet weak var guessesRemaining: UILabel!
     @IBOutlet weak var stackView: UIStackView!
     @IBOutlet weak var playAgain: UIButton!
+    @IBOutlet weak var congratulationsKey: UILabel!
+    
+    
     
     // Creates vaiables that allows extra information in the game and sets different aspecs (difficulty, Selecting the random number, and "deciding" how many guesses will be alloted
     var usedCounter: Int = 0
@@ -30,19 +33,29 @@ class Game: UIViewController {
         chosenRange.text = String("Guess a number from 0 to \(passedData.difficulty)") // Tells the used what range to guess from
         guessesRemaining.text = ("Guesses Remaining: \(passedData.counter)") // Tells the user how many guesses they will recieve.
         playAgain.isHidden = true
+        congratulationsKey.isHidden = true
         // Do any additional setup after loading the view.
     }
     
     
     @IBAction func submitButton(_ sender: Any) { // This will tell the computer to check the Users input and see how it should respond to the number it was given.
-        if Int(usersGuess.text!) != nil { // This will verify that the number was in fact a number.
-            if Int(usersGuess.text!)! > passedData.difficulty && Int(usersGuess.text!)! < 0 { // This checkes to make sure the number was inside of the given range.
-                InfoButton.text = ("Please choose a number inside the current range.")
-                }else if Int(usersGuess.text!) == passedData.randomNumber { // This will check to see if the users number is in fact the number that was randomly chosen at in the ViewController file.
-                    InfoButton.text = ("You guessed it Congratulations!")
-                    stackView.isHidden = true
-                    playAgain.isHidden = false // Makes an option appear for the user to play again
-            
+            if Int(usersGuess.text!) != nil { // This will verify that the number was in fact a number.
+                if Int(usersGuess.text!)! > passedData.difficulty || Int(usersGuess.text!)! < 0 { // This checkes to make sure the number was inside of the given range.
+                    InfoButton.text = ("Please choose a number inside the current range.")
+                } else if Int(usersGuess.text!) == passedData.randomNumber { // This will check to see if the users number is in fact the number that was randomly chosen at in the ViewController file.
+                    print(usersGuess, passedData.randomNumber)
+                        
+                    self.InfoButton.text = ("You guessed it Congratulations!")
+                    self.stackView.isHidden = true
+                    self.congratulationsKey.isHidden = false
+                    
+                    let sleepFor: UInt64 = 2 * 1_000_000_000
+                    
+                    DispatchQueue.main.asyncAfter(deadline: DispatchTime(uptimeNanoseconds: DispatchTime.now().uptimeNanoseconds + sleepFor)) { // sleeps for  2 seconds
+                        self.congratulationsKey.isHidden = true
+                        self.playAgain.isHidden = false // Makes an option appear for the user to play again
+                    }
+                    
                 }else if Int(usersGuess.text!)! < passedData.randomNumber &&  passedData.counter >= 1{ // Checks to see if the number you chose is lower than the random number.
                     InfoButton.text = ("Nope, thats to low.")
                     passedData.counter -= 1 // Takes one away from the the Users remaining guesses
@@ -50,7 +63,7 @@ class Game: UIViewController {
                     usedCounter += 1 // Adds one to the amount of guesses the user has used
                     guessesUsed.text = ("Guesses Used: \(usedCounter)")
                     usersGuess.text = ("") // Sets the user input back to nothing so that the user doesnt have to press bak on their previous guess
-                
+                    
                 }else if Int(usersGuess.text!)! > passedData.randomNumber &&  passedData.counter >= 1{// Checks to see if the number you chose is higher than the random number.
                     InfoButton.text = ("Nope thats too high")
                     passedData.counter -= 1 // Takes one away from the the Users remaining guesses
@@ -58,15 +71,16 @@ class Game: UIViewController {
                     usedCounter += 1 // adds one to the amount of guesses the user has used
                     guessesUsed.text = ("Guesses Used: \(usedCounter)")
                     usersGuess.text = ("") // Sets the user input back to nothing so that the user doesnt have to press bak on their previous guess
-                
+                    
                 }else{ // When the user runs out of guesses the text becomes 0 and it prints the text below...
                     usersGuess.text = ("-")
                     InfoButton.text = ("Well thats to Bad you lost")
+                    sleep(2)
                     stackView.isHidden = true
                     playAgain.isHidden = false
                 }
+            }else{
+                InfoButton.text = ("Please enter number")
             }
-        }
-
-    
+    }
 }
